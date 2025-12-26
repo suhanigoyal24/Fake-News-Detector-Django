b3cee085bba56a3a012254c179b7d585a382bea9
Suhani Goyal <163672403+suhanigoyal24@users.noreply.github.com>
2025-12-26 23:29:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,18 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Set the path to your Django project
+PROJECT_DIR="fakereader"
+
+# Navigate to the project directory
+cd $PROJECT_DIR
+
 # Collect static files
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Running migrations..."
-python3 manage.py migrate
+python manage.py migrate
 
 echo "Build script completed successfully."
